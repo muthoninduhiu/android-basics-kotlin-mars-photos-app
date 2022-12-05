9c0e5e27cb59f9d356440fc8d93a29ac353b2875
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-12-05 11:55:53 +0300

    add a Retrofit builder to build and create a Retrofit object

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,8 +1,14 @@
 package com.example.android.marsphotos.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
 
 //add base URL
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
 
+//add a Retrofit builder to build and create a Retrofit object
+private val retrofit = Retrofit.Builder()
+       
 class MarsApiService {
 }
