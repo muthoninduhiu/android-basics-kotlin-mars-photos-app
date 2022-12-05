900a997034def12b3c8d02de5838f3399b340c41
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-12-05 11:57:14 +0300

    add converter factory so JSON is output as a String

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -9,6 +9,8 @@ private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com
 
 //add a Retrofit builder to build and create a Retrofit object
 private val retrofit = Retrofit.Builder()
-       
+    //Retrofit object with the base URL and the converter factory.
+    .addConverterFactory(ScalarsConverterFactory.create())
+   
 class MarsApiService {
 }
