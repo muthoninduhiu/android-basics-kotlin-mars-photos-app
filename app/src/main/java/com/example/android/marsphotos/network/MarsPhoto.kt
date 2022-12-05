bfc22d3ef2c420246c9fbc67637bb7a3f974147d
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-12-05 12:50:58 +0300

    add @Json annotation

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -1,4 +1,8 @@
 package com.example.android.marsphotos.network
 
-class MarsPhoto {
-}
+import com.squareup.moshi.Json
+
+data class MarsPhoto(
+    val id: String,
+    @Json(name="image_src")val image_src: String
+)
