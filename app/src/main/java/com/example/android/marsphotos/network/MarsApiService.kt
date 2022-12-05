b11c8865c251c772fa06f0cb34ba6f91e4b3921b
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-12-05 12:56:20 +0300

    add moshi converter to use with retrofit

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,25 +1,29 @@
 package com.example.android.marsphotos.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.create
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
 //add base URL
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
-
+//add moshi
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 //add a Retrofit builder to build and create a Retrofit object
 private val retrofit = Retrofit.Builder()
     //Retrofit object with the base URL and the converter factory.
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create())
     //create retrofit object
     .build()
 
 //Create an interface that explains how Retrofit talks to our web server.
 interface MarsApiService {
     @GET("/photos")
-    suspend fun getPhotos() : String
+    suspend fun getPhotos() : List<MarsPhoto>
 }
 
 //add a lazily initialized retrofit object property
